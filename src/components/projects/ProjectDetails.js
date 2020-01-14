221b29cee37962584cd8feac58e0459f7f55c640
J-DK <dileepkumarjami@gmail.com>
2020-01-14 23:41:12 +0530

    fix(route): fixed the route issue by matching the EXACT component and also used the info from the automatically generated props

--- src/components/projects/ProjectDetails.js
+++ src/components/projects/ProjectDetails.js
@@ -1,11 +1,13 @@
 import React from "react";
 
-const ProjectDetails = () => {
+const ProjectDetails = (props) => {
+    // When this component is placed inside Router, automatically some props (info) is added
+    const {id} = props.match.params;
     return (
         <div className="container section project-details">
             <div className="card z-depth-9">
                 <div className="card-content">
-                    <span className="card-title">Project Title</span>
+                    <span className="card-title">Project Id: {id}</span>
                     <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
                 </div>
                 <div className="card-action grey lighten-4 grey-text">
